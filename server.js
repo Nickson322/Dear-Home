26d0af83c999388a2ce1f2ce4a67f1ae0c1f5b09
Nikita Patrakeev <coldrain4@rambler.ru>
2023-08-02 23:02:09 +0300

    0.0.2 Advanced rout. init

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const mysql   = require('mysql2');
 const app = express();
+const port = 3000;
 
 
 app.set('view engine', 'hbs');
 
-app.listen(3000);
 
+app.listen(port);
+
+
+console.log(`http://localhost:${port}`);
+
+
+//Basic routing
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+app.get('/sign', (req, res) => {
+    res.render('');
+})
+
+app.get('/register', (req, res) => {
+    res.render('');
+})
+
+
+
+//Advanced Routing
+const user_router = require('./routes/users');
+app.use("/users", user_router);
+
